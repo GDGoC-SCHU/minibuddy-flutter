4fe1a54d6ce92fe46567e9fff090a22c41918d62
y <dkgk2233@gmail.com>
2025-05-12 01:19:11 +0900

    feat 인터랙션 색상 회색계열로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ class MinibuddyApp extends StatelessWidget {
           brightness: Brightness.light,
           scaffoldBackgroundColor: Colors.white,
           primaryColor: Colors.grey[800],
+          splashColor: Colors.grey.withOpacity(0.2), // 터치 잔상 색
+          highlightColor: Colors.grey.withOpacity(0.1), // 누르고 있을 때 배경
+          hoverColor: Colors.grey.withOpacity(0.05), // (웹 등) 호버 시 색
           appBarTheme: AppBarTheme(
             backgroundColor: Colors.white,
             foregroundColor: Colors.grey[900],
@@ -30,17 +33,21 @@ class MinibuddyApp extends StatelessWidget {
               color: Colors.grey[900],
               fontSize: 20,
               fontWeight: FontWeight.bold,
+              fontFamily: 'Pretendard',
             ),
             iconTheme: IconThemeData(
               color: Colors.grey[800],
             ),
           ),
           textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Colors.black87),
-            bodyMedium: TextStyle(color: Colors.black54),
+            bodyLarge:
+                TextStyle(color: Colors.black87, fontFamily: 'Pretendard'),
+            bodyMedium:
+                TextStyle(color: Colors.black54, fontFamily: 'Pretendard'),
           ),
           colorScheme: ColorScheme.fromSwatch().copyWith(
             secondary: Colors.grey[400],
+            primary: Colors.grey[800], // 포커스 및 인터랙션 색
           ),
         ),
         routerConfig: router,
