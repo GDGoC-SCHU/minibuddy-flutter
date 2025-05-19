3f7489ca0da7d753f5359768150c2047468cbd18
y <dkgk2233@gmail.com>
2025-05-20 03:15:21 +0900

    feat: 토큰 발급 새걸로만

--- lib/services/auth/firebase_token_provider.dart
+++ lib/services/auth/firebase_token_provider.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 Future<String?> getIdToken() async {
   final user = FirebaseAuth.instance.currentUser;
   if (user != null) {
-    return await user.getIdToken(); // true면 강제 갱신도 가능
+    return await user.getIdToken(true); // 강제 새로 발급
   }
   return null;
 }
